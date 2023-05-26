0db772dd9a422f7fab6c3f96a9462ef524d6d432
antrioe <antrioe@gmali.com>
2023-05-26 17:36:19 +0800

    fix

--- astro.config.js
+++ astro.config.js
@@ -51,46 +51,44 @@ export default defineConfig({
       vueTemplate: true,
     }),
     // https://vite-pwa-org.netlify.app/frameworks/astro.html
-    process.env.OUTPUT === 'netlify'
-      ? undefined
-      : AstroPWA({
-        mode: 'development',
-        base: '/',
-        scope: '/',
-        includeAssets: ['favicon.svg'],
-        registerType: 'autoUpdate',
-        manifest: {
-          name: 'Astro PWA',
-          short_name: 'Astro PWA',
-          theme_color: '#ffffff',
-          icons: [
-            {
-              src: 'pwa-192x192.png',
-              sizes: '192x192',
-              type: 'image/png',
-            },
-            {
-              src: 'pwa-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-            },
-            {
-              src: 'pwa-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'any maskable',
-            },
-          ],
-        },
-        workbox: {
-          navigateFallback: '/404',
-          globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
-        },
-        devOptions: {
-          enabled: true,
-          navigateFallbackAllowlist: [/^\/404$/],
-        },
-      }),
+    AstroPWA({
+      mode: 'development',
+      base: '/',
+      scope: '/',
+      includeAssets: ['favicon.svg'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Astro PWA',
+        short_name: 'Astro PWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: '/404',
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\/404$/],
+      },
+    }),
   ],
   output: 'server',
   adapter: envAdapter(),
